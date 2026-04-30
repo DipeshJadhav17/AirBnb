fa961f52e5e0ee2f51c02d5da59665fccc143f00
Dipesh Jadhav <dipeshjadhav065@gmail.com>
2026-04-30 12:56:46 +0530

    fix mongodb data

--- init/index.js
+++ init/index.js
@@ -1,11 +1,30 @@
+require('dotenv').config({ path: '../.env' });
+
+
+
 const mongoose = require('mongoose');
 const initData = require('./data.js');
 const Listing = require('../models/listing.js');
 
 
-
 // MongoDB connection URL
-const mongo_url = 'mongodb://127.0.0.1:27017/AirBNB_clone';
+const mongo_url = process.env.ATLAS_URL;
+
+async function main() {
+    await mongoose.connect(mongo_url);
+    // use `await mongoose.connect('mongodb://user:password@
+    await initDB();
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => {
+        return { ...obj, owner: "69d3532e45fe35768f2e94b1" };
+    });
+    await Listing.insertMany(initData.data);
+    //await Listing.deleteMany({title: "Happy Home"});
+    console.log("Database initialized with sample data");
+}
 
 main().then(() => {
     console.log("Connected to MongoDB successfully");
@@ -13,19 +32,4 @@ main().then(() => {
     console.log(err)
 });
 
-async function main() {
-  await mongoose.connect(mongo_url);
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/wanderlust');` if your database has auth enabled
-}
-
-const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => {
-        return { ...obj, owner: "69d3532e45fe35768f2e94b1"};
-    });
-    await Listing.insertMany(initData.data);
-    //await Listing.deleteMany({title: "Happy Home"});
-    console.log("Database initialized with sample data");
-}
-
-initDB();
+console.log("ENV:", process.env.ATLAS_URL);
